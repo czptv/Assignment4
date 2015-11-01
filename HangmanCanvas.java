429949abaea78a331fb5ea207b83ce64428cbbc9
S <s>
2015-11-01 03:40:01 -0800

    {"type":"TI", "runs":"1"}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -170,7 +170,7 @@ public class HangmanCanvas extends GCanvas {
 	private static final int HIP_WIDTH = 36;
 	private static final int LEG_LENGTH = 108;
 	private static final int FOOT_LENGTH = 28;
-	private static final int DIFF=100; //how higher than the center the person is display
+	private static final int DIFF=50; //how higher than the center the person is display
 	
 // Instance variables
 	private GLabel display;
